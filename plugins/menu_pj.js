096b157363c79810fe25794fa226896069c281bf
Felipebali <felipebaliski@gmail.com>
2026-03-23 10:43:16 -0300

    Update menu_pj.js

--- plugins/menu_pj.js
+++ plugins/menu_pj.js
@@ -22,8 +22,8 @@ let handler = async (m, { conn }) => {
 ┃ 💔 *.drop*
 ┃ ➤ Liberar tu personaje
 ┃
-┃ 🔄 *.cambiar <nombre>*
-┃ ➤ Cambiar personaje manualmente
+┃ 🔄 *.cambiar*
+┃ ➤ Cambiar personaje automáticamente
 ┃
 ┃━━━━━━━━━━━━━━
 ┃ 👑 *PODER DE LOS OWNERS*

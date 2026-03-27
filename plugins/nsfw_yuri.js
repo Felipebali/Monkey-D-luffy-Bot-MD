8cf8d52df38f3af9b129bb5742f340537ef6686f
Felipebali <felipebaliski@gmail.com>
2026-03-27 16:33:59 -0300

    Update nsfw_yuri.js

--- plugins/nsfw_yuri.js
+++ plugins/nsfw_yuri.js
@@ -5,7 +5,7 @@ import path from 'path';
 
 let handler = async (m, { conn, usedPrefix }) => {
 if (!db.data.chats[m.chat].nsfw && m.isGroup) {
-    return m.reply(`${emoji} El contenido *NSFW* está desactivado en este grupo.\n> Un administrador puede activarlo con el comando » *#nsfw on*`);
+    return m.reply(`${emoji} El contenido *NSFW* está desactivado en este grupo.\n> Un dueño puede activarlo con el comando » *.nsfw*`);
     }
 
     let who;
